e67f3020c21fb26c58d821898cb5c8f38855f202
raahul <venkatraahul99@gmail.com>
2021-04-28 18:01:43 +0530

    first commit

--- src/app/allModules/pages/preview/preview.service.ts
+++ src/app/allModules/pages/preview/preview.service.ts
@@ -11,40 +11,61 @@ import { Observable } from 'rxjs';
 })
 export class PreviewService {
 
+}
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Q_Projects } from 'app/models/project';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreviewService {
+
   constructor(private http: HttpClient) { }
-  errorHandler(httpErrorResponse: HttpErrorResponse): Observable<string> {
-    return throwError(httpErrorResponse.error || httpErrorResponse.message || 'Something Went Wrong');
-  }
   private baseUrl = 'http://localhost:55997/';
   postdatatoQ_Project(doc: any): Observable<any> {
-    console.log("doc", doc);
 
     // return this.http.post<any>(this.baseUrl + "api/Create/AddQ_Project", doc);
     return this.http.post<any>(this.baseUrl + "api/Create/AddQ_Project", doc, {
-      headers: new HttpHeaders({
-        'Content-type': 'application/json'
-      })
+      // headers: new HttpHeaders({
+      //   'Content-type': 'application/json'
+      // })
     })
-
-    
   }
-  PostEmailData(comm:any): Observable<any | Common>{
-    var email1={
-      EmailID:comm.email1,
-      EmailIDD:comm.email2
-    }
-    // return this.http.post<any>(`http://localhost:55997/api/Email/SendEmail`,abc)
-    return this.http.post<any>(this.baseUrl + "api/Email/sendmail1",email1,{
-      headers: new HttpHeaders({
-        'Content-type': 'application/json'
-      })
+  postdatatoQ_Project_Group_Map(doc: any): Observable<any> {
+
+    return this.http.post<any>(this.baseUrl + "api/Create/AddQ_project_GroupMap", doc, {
     })
-    .pipe(catchError(this.errorHandler));
-    
   }
+  postdatatoQ_Questions(doc: any): Observable<any> {
 
-  GetRoleWithEmail(): Observable<any | string>{
-    return this.http.get<any>(`http://localhost:7010/authenticationapi/Master/GetRoleWithEmail`)
-    
+    return this.http.post<any>(this.baseUrl + "api/Create/AddQ_Questions", doc, {
+    })
+  }
+  postdatatoQ_Answers(doc: any): Observable<any> {
+
+    return this.http.post<any>(this.baseUrl + "api/Create/AddQ_Answers", doc, {
+    })
+  }
+  postdatatoQ_AnswersType_Master(doc: any): Observable<any> {
+
+    return this.http.post<any>(this.baseUrl + "api/Create/AddQ_AnswersType", doc, {
+    })
+  }
+  postdatatoQuestionGroups(doc: any): Observable<any> {
+
+    return this.http.post<any>(this.baseUrl + "api/Create/AddQuestionGroup", doc, {
+      // headers: new HttpHeaders({
+      //   'Content-type': 'application/json'
+      // })
+    })
+  }
+  postdatatoQ_Answer(doc: any): Observable<any> {
+    return this.http.post<any>(this.baseUrl + "api/Create/AddQ_Answers", doc, {
+      // headers: new HttpHeaders({
+      //   'Content-type': 'application/json'
+      // })
+    })
   }
 }
